50642482b490acbc87ffdf21215ed70f127cad09
Maera <baeitsmae@gmail.com>
2023-07-06 15:12:51 +0200

    Medias queries

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,17 @@ import { Link } from "react-router-dom"
 
 
 function Home() {
-
     return (
-       
-                <section className="home">
-                <h2 className="displayNone">title</h2>
-                    <div>
-                        <p>
-                            Plongez dans le paradis des saveurs. <br></br>
-                            Un rendez-vous avec la délicatesse, un festin pour les sens...
-                        </p>
-                        <Link to="/menu" className="button">Voir la carte</Link>
-                    </div>
-                </section>
+        <section className="home">
+            <h2 className="displayNone">title</h2>
+            <div>
+                <p>
+                    Plongez dans le paradis des saveurs. <br></br>
+                    Un rendez-vous avec la délicatesse, un festin pour les sens...
+                </p>
+                <Link to="/menu" className="button">Voir la carte</Link>
+            </div>
+        </section>
          
     )
 }
