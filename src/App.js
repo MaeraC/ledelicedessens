901343b5e4d1d05f0d150b9cff4e3f4b52a15793
Maera <baeitsmae@gmail.com>
2023-07-06 11:56:19 +0200

    Project initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Nav from './components/Nav' 
+import Footer from './components/Footer'
+import MenuCards from './pages/MenuCards'
+import Galerie from './pages/Galerie'
+import Reservations from './pages/Reservation'
+import Restaurant from './pages/Restaurant'
+import Error from './pages/Error'
+import LaCarte from './pages/LaCarte'
+import Loader from './components/Loader'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Router>
+            <Loader />
+                <Nav />
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/menu" element={<MenuCards />} />
+                    <Route path="/la-carte/:typeOfCard" element={<LaCarte />} />
+                    <Route path="/galerie" element={<Galerie />} />
+                    <Route path="/reservations" element={<Reservations />} />
+                    <Route path="/restaurant" element={<Restaurant />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </>
+    )
 }
 
-export default App;
+export default App
